becde61843461ee645b231ec283c4981511afcd2
EmanQudah <isalqudah174@cit.just.edu.jo>
2023-05-21 21:02:26 +0300

    Five Test Cases

---
+++ src/Parameters.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Parameters {
+	WebDriver driver = new ChromeDriver(); 
+	String Homepage = "https://www.demoblaze.com/";
+	String UserName1 = ("University44");
+	String Password1 = ("ABcd@22$");
+}
